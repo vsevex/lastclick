bc185b514d20e809910c77c65bcc1473b264949d
vsevex <vsevex@gmail.com>
2026-02-20 16:23:52 +0400

    add engine module exports: create index.ts to consolidate and export key components and types from GameEngine, VolatilityModel, and types, streamlining module access for the game engine

---
+++ web/src/engine/index.ts
@@ -0,0 +1,13 @@
+export { GameEngine } from "./GameEngine";
+export { VolatilityModel } from "./VolatilityModel";
+export {
+  RoundState,
+  PlayerState,
+  DEFAULT_ENGINE_CONFIG,
+  type EngineRoom,
+  type EnginePlayer,
+  type GameEvent,
+  type DebugCommand,
+  type EngineConfig,
+  type EngineSnapshot,
+} from "./types";
